3120b746f91e4e78dd881c87f66059ac98bd8336
ruchitate <ruchitate@hey.jp>
2022-10-07 12:08:34 +0900

    client, serverのモジュール追加

---
+++ client/src/test/java/com/b1a9idps/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1a9idps.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
